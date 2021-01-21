f5300996287fc886ab7746e9ff9c02ad8bae9a8d
chenshijian <chenshijian22@gmail.com>
2021-01-21 22:41:40 +0800

    最后一次

---
+++ Eternal/Form_Love.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Eternal
+{
+    partial class Form_Love
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Love));
+            this.trackBar_Range = new System.Windows.Forms.TrackBar();
+            this.panel_Draw = new System.Windows.Forms.Panel();
+            this.label_R = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button_BackgroundColor = new System.Windows.Forms.Button();
+            this.button_Foreground = new System.Windows.Forms.Button();
+            this.panel_information = new System.Windows.Forms.Panel();
+            this.button_Reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_PenWd = new System.Windows.Forms.ComboBox();
+            this.button_DropOut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Range)).BeginInit();
+            this.panel_information.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar_Range
+            // 
+            this.trackBar_Range.AutoSize = false;
+            this.trackBar_Range.BackColor = System.Drawing.Color.White;
+            this.trackBar_Range.LargeChange = 1;
+            this.trackBar_Range.Location = new System.Drawing.Point(409, 52);
+            this.trackBar_Range.Maximum = 150000;
+            this.trackBar_Range.Name = "trackBar_Range";
+            this.trackBar_Range.Size = new System.Drawing.Size(30, 20);
+            this.trackBar_Range.TabIndex = 0;
+            this.trackBar_Range.TickFrequency = 150;
+            this.trackBar_Range.ValueChanged += new System.EventHandler(this.trackBar_Range_ValueChanged);
+            // 
+            // panel_Draw
+            // 
+            this.panel_Draw.BackColor = System.Drawing.Color.White;
+            this.panel_Draw.Location = new System.Drawing.Point(13, 78);
+            this.panel_Draw.Name = "panel_Draw";
+            this.panel_Draw.Size = new System.Drawing.Size(487, 214);
+            this.panel_Draw.TabIndex = 1;
+            this.panel_Draw.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Draw_Paint);
+            // 
+            // label_R
+            // 
+            this.label_R.AutoSize = true;
+            this.label_R.Location = new System.Drawing.Point(3, 3);
+            this.label_R.Name = "label_R";
+            this.label_R.Size = new System.Drawing.Size(17, 12);
+            this.label_R.TabIndex = 4;
+            this.label_R.Text = "R:";
+            // 
+            // button_BackgroundColor
+            // 
+            this.button_BackgroundColor.BackColor = System.Drawing.Color.Transparent;
+            this.button_BackgroundColor.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button_BackgroundColor.FlatAppearance.BorderSize = 0;
+            this.button_BackgroundColor.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button_BackgroundColor.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.button_BackgroundColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_BackgroundColor.Location = new System.Drawing.Point(115, -1);
+            this.button_BackgroundColor.Name = "button_BackgroundColor";
+            this.button_BackgroundColor.Size = new System.Drawing.Size(52, 20);
+            this.button_BackgroundColor.TabIndex = 5;
+            this.button_BackgroundColor.Text = "背景色";
+            this.button_BackgroundColor.UseVisualStyleBackColor = false;
+            this.button_BackgroundColor.Click += new System.EventHandler(this.button_BackgroundColor_Click);
+            // 
+            // button_Foreground
+            // 
+            this.button_Foreground.BackColor = System.Drawing.Color.Transparent;
+            this.button_Foreground.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button_Foreground.FlatAppearance.BorderSize = 0;
+            this.button_Foreground.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button_Foreground.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.button_Foreground.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Foreground.Location = new System.Drawing.Point(57, -1);
+            this.button_Foreground.Name = "button_Foreground";
+            this.button_Foreground.Size = new System.Drawing.Size(52, 20);
+            this.button_Foreground.TabIndex = 6;
+            this.button_Foreground.Text = "前景色";
+            this.button_Foreground.UseVisualStyleBackColor = false;
+            this.button_Foreground.Click += new System.EventHandler(this.button_Foreground_Click);
+            // 
+            // panel_information
+            // 
+            this.panel_information.BackColor = System.Drawing.Color.White;
+            this.panel_information.Controls.Add(this.button_Reset);
+            this.panel_information.Controls.Add(this.label1);
+            this.panel_information.Controls.Add(this.comboBox_PenWd);
+            this.panel_information.Controls.Add(this.button_BackgroundColor);
+            this.panel_information.Controls.Add(this.label_R);
+            this.panel_information.Controls.Add(this.button_Foreground);
+            this.panel_information.Location = new System.Drawing.Point(200, 18);
+            this.panel_information.Name = "panel_information";
+            this.panel_information.Size = new System.Drawing.Size(300, 20);
+            this.panel_information.TabIndex = 7;
+            // 
+            // button_Reset
+            // 
+            this.button_Reset.BackColor = System.Drawing.Color.Transparent;
+            this.button_Reset.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button_Reset.FlatAppearance.BorderSize = 0;
+            this.button_Reset.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button_Reset.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.button_Reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Reset.Location = new System.Drawing.Point(248, 0);
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.Size = new System.Drawing.Size(52, 22);
+            this.button_Reset.TabIndex = 9;
+            this.button_Reset.Text = "重置";
+            this.button_Reset.UseVisualStyleBackColor = false;
+            this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "笔宽：";
+            // 
+            // comboBox_PenWd
+            // 
+            this.comboBox_PenWd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_PenWd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox_PenWd.FormattingEnabled = true;
+            this.comboBox_PenWd.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBox_PenWd.Location = new System.Drawing.Point(209, 0);
+            this.comboBox_PenWd.Name = "comboBox_PenWd";
+            this.comboBox_PenWd.Size = new System.Drawing.Size(35, 20);
+            this.comboBox_PenWd.TabIndex = 7;
+            this.comboBox_PenWd.SelectedIndexChanged += new System.EventHandler(this.comboBox_PenWd_SelectedIndexChanged);
+            // 
+            // button_DropOut
+            // 
+            this.button_DropOut.BackColor = System.Drawing.Color.Transparent;
+            this.button_DropOut.FlatAppearance.BorderSize = 0;
+            this.button_DropOut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.button_DropOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_DropOut.ForeColor = System.Drawing.Color.Red;
+            this.button_DropOut.Location = new System.Drawing.Point(40, 21);
+            this.button_DropOut.Name = "button_DropOut";
+            this.button_DropOut.Size = new System.Drawing.Size(52, 20);
+            this.button_DropOut.TabIndex = 8;
+            this.button_DropOut.Text = "退出";
+            this.button_DropOut.UseVisualStyleBackColor = false;
+            this.button_DropOut.Visible = false;
+            this.button_DropOut.Click += new System.EventHandler(this.button_DropOut_Click);
+            // 
+            // Form_Love
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(540, 353);
+            this.Controls.Add(this.button_DropOut);
+            this.Controls.Add(this.panel_information);
+            this.Controls.Add(this.trackBar_Range);
+            this.Controls.Add(this.panel_Draw);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_Love";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Eternal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form_Love_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Range)).EndInit();
+            this.panel_information.ResumeLayout(false);
+            this.panel_information.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar_Range;
+        private System.Windows.Forms.Panel panel_Draw;
+        private System.Windows.Forms.Label label_R;
+        public System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button_BackgroundColor;
+        private System.Windows.Forms.Button button_Foreground;
+        private System.Windows.Forms.Panel panel_information;
+        private System.Windows.Forms.ComboBox comboBox_PenWd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Reset;
+        private System.Windows.Forms.Button button_DropOut;
+    }
+}
---
+++ Eternal/Form_Love.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eternal
+{
+    public partial class Form_Love : Form
+    {
+        private Color penColor=Color.Red;
+        private Color backColor = Color.White;
+        private int penWd = 1;
+        private double a;
+        
+        public Form_Love()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Love_Load(object sender, EventArgs e)
+        {
+            int SH = Screen.PrimaryScreen.Bounds.Height;
+            int SW = Screen.PrimaryScreen.Bounds.Width;
+            this.Width = SW;
+            this.Height = SH;
+            int X =SW - 350;
+            int Y =  60;
+            trackBar_Range.SetBounds(X,Y,300,25);
+
+            Y = 30;
+            panel_information.SetBounds(X, Y, 300, 20);
+
+            X =30;
+            Y = 100;
+            panel_Draw.SetBounds(X,Y,this.Width-60,this.Height-150);
+
+            X = this.Width - 52; 
+            Y = this.Height - button_DropOut.Height;
+            button_DropOut.SetBounds(X, Y, 52, 20);
+
+            comboBox_PenWd.SelectedIndex = 0;
+            label_R.Text = "R:" + 0;
+
+            if (panel_Draw.Height<450 || panel_Draw.Width<500)
+            {
+                MessageBox.Show("屏幕分辨率不足！","请调节屏幕分辨率", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void trackBar_Range_ValueChanged(object sender, EventArgs e)
+        {
+            a = (trackBar_Range.Value) / 1000.0;
+            label_R.Text = "R:" + a;
+            panel_Draw.Refresh();
+        }
+
+        private void panel_Draw_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            //X范围宽度
+            int ch = 181;
+            Point[] points1 = new Point[2 * ch + 1];
+            Point[] points = new Point[2 * ch + 1];
+            int xMultiple = 135;
+            int yMultiple = 110;
+            double x;
+            double y;
+            for (int i = 0; i < points.Length; i++)
+            {
+                a = (trackBar_Range.Value)/1000.0;
+                if (a == 100.0)
+                {
+                    a = 50;
+                }
+                x = (i-ch)/100.0;
+                y = -Math.Pow(x*x, 1.0 / 3.0) + 0.9 * Math.Sqrt(3.3 - x * x) * Math.Sin(a * Math.PI * x);
+                
+                points[i] = new Point(Convert.ToInt32(x*xMultiple), Convert.ToInt32(y*yMultiple));
+            }
+
+            a = (trackBar_Range.Value) / 1000.0;
+            if (a == 150.0)
+            {
+                int X, Y;
+                X = panel_Draw.Width / 2 - 8;
+                Y = panel_Draw.Height / 2 - 5;
+                button_DropOut.Visible = true;
+                
+
+                int b = 150;
+                for (int i = 0; i < points1.Length; i++)
+                {
+                    x = (i - ch) / 100.0;
+                    y = -Math.Pow(x * x, 1.0 / 3.0) + 0.9 * Math.Sqrt(3.3 - x * x) * Math.Sin(b * Math.PI * x);
+                    points1[i] = new Point(Convert.ToInt32(x * 13.5), Convert.ToInt32(y *11));
+                }
+            }
+
+            //设置笔
+            Pen mypen = new Pen(penColor, penWd);
+            System.Drawing.Drawing2D.AdjustableArrowCap arrow = new AdjustableArrowCap(8, 8, false);
+
+            //获取绘制面板的中心
+            //int halfWidth = panel_Draw.Width / 2;
+            //int halfHeigh = panel_Draw.Height / 2;
+            //绘制坐标轴
+            //g.DrawLine(mypen, 7, halfHeigh, panel_Draw.Width - 7, halfHeigh);
+            //g.DrawLine(mypen, halfWidth, panel_Draw.Height - 5, halfWidth, 5);
+
+
+            //改变坐标原点.通过使此 System.Drawing.Graphics 的变换矩阵左乘指定的平移来更改坐标系统的原点。
+            g.TranslateTransform(panel_Draw.Width / 2, panel_Draw.Height / 2+50);
+            g.DrawLines(mypen, points);
+            if(a==150.0)
+            {
+                g.DrawLines(mypen, points1);
+            }
+        }
+
+        //设置背景色
+        private void button_BackgroundColor_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = colorDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                backColor= colorDialog1.Color;
+                this.BackColor = backColor;
+                this.panel_Draw.BackColor = backColor;
+                panel_Draw.Refresh();
+            }
+        }
+
+        //设置前景色
+        private void button_Foreground_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = colorDialog1.ShowDialog();
+            //如果选中颜色，单击“确定”按钮则改变文本框的文本颜色
+            if (dr == DialogResult.OK)
+            {
+               penColor= colorDialog1.Color;
+               panel_Draw.Refresh();
+            }
+        }
+
+        //设置笔宽
+        private void comboBox_PenWd_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            penWd = comboBox_PenWd.SelectedIndex+1;
+            panel_Draw.Refresh();
+        }
+
+        //重置
+        private void button_Reset_Click(object sender, EventArgs e)
+        {
+            penColor = Color.Red;
+            backColor = Color.White;
+            this.BackColor = backColor;
+            this.panel_Draw.BackColor = backColor;
+            comboBox_PenWd.SelectedIndex = 0;
+            penWd = comboBox_PenWd.SelectedIndex + 1;
+            trackBar_Range.Value = 0;
+            a = (trackBar_Range.Value) / 1000.0;
+            label_R.Text = "R:" + a;
+            this.button_DropOut.Visible = false;
+        }
+
+        private void button_DropOut_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
